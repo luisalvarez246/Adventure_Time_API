051b01d06bbf0eaf9874c4603242ed4e30c90554
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-08-04 14:01:00 +0200

    main commit

---
+++ src/main/java/com/adventure_time/AdventureTimeApplication.java
@@ -0,0 +1,13 @@
+package com.adventure_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adventure_time/AdventureTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventure_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
