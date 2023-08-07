6933d87e2501f3142059c99bda42d5c2cd2964f1
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-08-07 21:50:53 +0200

    added GET method

--- src/main/java/com/adventure_time/controller/CharactersController.java
+++ src/main/java/com/adventure_time/controller/CharactersController.java
@@ -1,6 +1,23 @@
 package com.adventure_time.controller;
 
+import com.adventure_time.model.CharacterAT;
+import com.adventure_time.services.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/characters")
 public class CharactersController
 {
+    @Autowired
+    CharacterService    characterService;
 
+    @GetMapping
+    public ArrayList<CharacterAT> getAllCharacters ()
+    {
+        return (characterService.getAllCharacters());
+    }
 }
