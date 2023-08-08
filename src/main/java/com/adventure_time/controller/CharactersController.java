e6555be8f6dac4a3b4e4f02e31e5422e4528091b
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-08-08 08:56:00 +0200

    added getCharacterById to controller, working fine.

--- src/main/java/com/adventure_time/controller/CharactersController.java
+++ src/main/java/com/adventure_time/controller/CharactersController.java
@@ -3,9 +3,8 @@ package com.adventure_time.controller;
 import com.adventure_time.model.CharacterAT;
 import com.adventure_time.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 
 @RestController
@@ -16,8 +15,14 @@ public class CharactersController
     CharacterService    characterService;
 
     @GetMapping
-    public ArrayList<CharacterAT> getAllCharacters ()
+    public  ArrayList<CharacterAT> getAllCharacters()
     {
         return (characterService.getAllCharacters());
+    }
+
+    @GetMapping(path = "/{id}")
+    public  CharacterAT getCharacterById(@PathVariable int id)
+    {
+        return (characterService.getCharacterById(id));
     }
 }
